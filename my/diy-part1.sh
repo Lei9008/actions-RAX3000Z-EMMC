a5ee35f9f0d06b4d821f51f4f53e0d378229b645
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-25 09:57:30 +0800

    Update diy-part1.sh

--- my/diy-part1.sh
+++ my/diy-part1.sh
@@ -24,15 +24,27 @@
 git clone https://github.com/linkease/istore-ui package/istore-ui
 git clone https://github.com/linkease/istore package/istore
 
+# 应用过滤
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+# 简单MESH
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+
+# 集客AC控制器
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+
+# AdguardHome-app
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# MosDns
+git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+
+# alist文件列表
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
+
 # 主题皮肤
 #git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 #git clone https://github.com/derisamedia/luci-theme-alpha package/luci-theme-alpha
 #git clone https://github.com/SAENE/luci-theme-design package/luci-theme-design
-# 集客AC控制器
-git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 
-# alist文件列表
-#git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
-# MosDns
-git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
