5cabf0ba1ba4f077ca795ff699384194f039c9b5
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-25 10:25:14 +0800

    Update diy-part2.sh

--- simplify/diy-part2.sh
+++ simplify/diy-part2.sh
@@ -10,6 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#web登陆密码从空密码修改为 password
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+#修改主机名
+sed -i "s/hostname='OpenWrt'/hostname='RAX3000Z'/g" package/base-files/files/bin/config_generate
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Del duplicate packages------------------
