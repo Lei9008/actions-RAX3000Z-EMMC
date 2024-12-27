6c7833e9188f6b9f596f6c9d6d6bb8b468f214bb
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-27 15:21:25 +0800

    Update diy-part2.sh

--- routine/diy-part2.sh
+++ routine/diy-part2.sh
@@ -11,17 +11,17 @@
 #
 
 #修改默认IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 #web登陆密码从空密码修改为 password
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #修改主机名
-sed -i "s/hostname='OpenWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
+#sed -i "s/hostname='OpenWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
 
 
 ##-----------------Del duplicate packages------------------
-rm -rf feeds/packages/net/open-app-filter
+#rm -rf feeds/packages/net/open-app-filter
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
