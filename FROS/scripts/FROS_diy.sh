8c74f30e8f819929b7e1bcde9ac2e1e14dda414b
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-11 07:41:38 +0800

    Update FROS_diy.sh

--- FROS/scripts/FROS_diy.sh
+++ FROS/scripts/FROS_diy.sh
@@ -11,9 +11,6 @@ sed -i "s/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 #修改主机名
 sed -i "s/hostname='.*'/hostname='RAX3000Z'/g" package/base-files/files/bin/config_generate
 
-##添加自己的插件库
-echo -e "\nsrc-git Lei9008_package https://github.com/Lei9008/openwrt_package_Lite" >> feeds.conf.default
-
 ##WiFi
 #sed -i "s/MT7981_AX3000_2.4G/HiWiFi/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
 #sed -i "s/MT7981_AX3000_5G/HiWiFi_5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
@@ -22,66 +19,6 @@ echo -e "\nsrc-git Lei9008_package https://github.com/Lei9008/openwrt_package_Li
 sed -i "s/ImmortalWrt-2.4G/HiWiFi/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "s/ImmortalWrt-5G/HiWiFi_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-###############
-cat <<EOL >> package/base-files/files/etc/uci-defaults/99_Lei9008_settings
-#!/bin/sh
-
-# 设置主机名
-# uci set system.@system[0].hostname='MzWrt'
-
-# 修改默认IP
-# uci set network.lan.ipaddr='192.168.10.1'
-
-# 设置 2.4GHz 频段的国家为中国
-# uci set wireless.radio0.country='CN'
-# 设置 5GHz 频段的国家为中国
-# uci set wireless.radio1.country='CN'
-
-# 信道设置
-# uci set wireless.radio0.channel='auto'  # 设置 2.4GHz 信道为 13
-# uci set wireless.radio1.channel='auto'  # 设置 5GHz 信道为 36
-
-# 设置 2.4GHz 频段的无线模式（支持 802.11n，20MHz 或 40MHz，最大带宽 40MHz）
-# uci set wireless.radio0.htmode='HE40'  # 设置 2.4GHz 频段为 40MHz 带宽
-
-# 设置 5GHz 频段的无线模式为 HE160（最大带宽 160MHz）
-# uci set wireless.radio1.htmode='HE160'  # 设置 5GHz 带宽为 160MHz
-
-# 开启 Beamforming 功能
-uci set wireless.radio0.beamforming='1'  # 开启 2.4GHz 的 Beamforming
-uci set wireless.radio1.beamforming='1'  # 开启 5GHz 的 Beamforming
-
-# 开启 MU-MIMO 支持
-uci set wireless.radio0.mu_mimo='1'  # 开启 2.4GHz 的 MU-MIMO
-uci set wireless.radio1.mu_mimo='1'  # 开启 5GHz 的 MU-MIMO
-
-# 开启 OFDMA 支持
-uci set wireless.radio0.ofdma='1'    # 开启 2.4GHz 的 OFDMA
-uci set wireless.radio1.ofdma='1'    # 开启 5GHz 的 OFDMA
-
-# 设置 2.4GHz SSID 名称为 MzWrt-2.4G
-# uci set wireless.default_radio0.ssid='MzWrt-2.4G'
-
-# 设置 5GHz SSID 名称为 MzWrt-5G
-# uci set wireless.default_radio1.ssid='MzWrt-5G'
-
-# 提交无线配置
-uci commit wireless
-
-# 重启网络
-# /etc/init.d/network restart
-
-# 提交修改的默认IP
-# uci commit network
-
-# 如果您确实需要重启系统或修改主机名，也可以执行
-# 提交主机名配置
-# uci commit system
-# 重启系统
-# /etc/init.d/system restart
-
-EOL
-################
 
 ##-----------------Del duplicate packages------------------
 #rm -rf feeds/packages/net/open-app-filter
