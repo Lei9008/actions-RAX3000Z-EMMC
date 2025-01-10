36273585c894839b75a3cbce1c6d0fdc9465311c
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-11 07:40:29 +0800

    Create diy1.sh

---
+++ FROS/scripts/diy1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# Lei9008 script
+#================================================= 
+
+
+##添加自己的插件库
+echo -e "\nsrc-git Lei9008_package https://github.com/Lei9008/openwrt_package_Lite" >> feeds.conf.default
