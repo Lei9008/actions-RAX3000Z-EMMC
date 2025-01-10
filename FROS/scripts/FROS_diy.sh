7ea6d9230557379ae1559fe2c79983d679094993
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-10 20:40:20 +0800

    Update FROS_diy.sh

--- FROS/scripts/FROS_diy.sh
+++ FROS/scripts/FROS_diy.sh
@@ -9,12 +9,10 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i "s/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g" package/base-files/files/etc/shadow
 
 #修改主机名
-#sed -i "s/hostname='ImmortalWrt'/hostname='Rax3000z'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='RAX3000Z'/g" package/base-files/files/bin/config_generate
 
 ##添加自己的插件库
 # echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default
-##更改主机名
-sed -i "s/hostname='.*'/hostname='RAX3000Z'/g" package/base-files/files/bin/config_generate
 
 ##WiFi
 #sed -i "s/MT7981_AX3000_2.4G/HiWiFi/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
