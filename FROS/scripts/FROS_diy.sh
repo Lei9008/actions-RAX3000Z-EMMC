4915c90b8430a75ae38098f610317de134324914
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-23 06:36:38 +0800

    Update FROS_diy.sh

--- FROS/scripts/FROS_diy.sh
+++ FROS/scripts/FROS_diy.sh
@@ -47,6 +47,8 @@ sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/
 #echo -e "\nsrc-git Lei9008_package https://github.com/Lei9008/openwrt_package_Lite" >> feeds.conf.default
 ## clone kiddin9/openwrt-packages仓库
 git clone https://github.com/kiddin9/kwrt-packages package/new/openwrt-packages
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/liker5092/extra_ipk" >> feeds.conf.default
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
