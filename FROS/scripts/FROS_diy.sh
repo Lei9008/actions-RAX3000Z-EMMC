674c39b62b98cd1d7982e820a715538b31967ca4
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-03-11 12:24:46 +0800

    Update FROS_diy.sh

--- FROS/scripts/FROS_diy.sh
+++ FROS/scripts/FROS_diy.sh
@@ -14,9 +14,13 @@ sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='LeiWrt-$(date +%Y%m%d)'
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By Lei9008'/g" package/base-files/files/etc/openwrt_release
 #cp -af feeds/extraipk/patch/diy/banner-Leiwrt  package/base-files/files/etc/banner
 
+
 ##修改WiFi名
 sed -i "s/ImmortalWrt-2.4G/HiWiFi/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "s/ImmortalWrt-5G/HiWiFi_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+sed -i "s/MT7981_AX3000_2.4G/HiWiFi/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7981_AX3000_5G/HiWiFi_5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 
 #############
 
